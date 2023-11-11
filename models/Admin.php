d01f55000ef33de13644089329f1796441cf59db
Jhonathan Nila <nilalimon@gmail.com>
2023-11-10 23:46:50 -0600

    Se migra el login a MVC y se valida ssi el formulario esta vacío y si existe o no el usuario

---
+++ models/Admin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Model;
+
+class Admin extends ActiveRecord {
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'email', 'password'];
+
+    public $id;
+    public $email;
+    public $password;
+
+    public function __construct($args = []) {
+        $this->id = $args['id'] ?? null;
+        $this->email = $args['email'] ?? null;
+        $this->password = $args['password'] ?? null;
+    }
+
+    public function validar() {
+        if(!$this->email) {
+            self::$errores[] = 'El Email es obligatorio';
+        }
+        if(!$this->password) {
+            self::$errores[] = 'El Password es obligatorio';
+        }
+        return self::$errores;
+    }
+    public function existeUsuario() {
+        $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1";
+        $resultado = self::$db->query($query);
+        if(!$resultado->num_rows) {
+            self::$errores[] = 'El usuario no existe';
+            return;
+        }
+        return $resultado;
+    }
+}
